74656f7d2d9bc270799e1eb27c81f36027915917
daisyy125 <daisy125@gmail.com>
2020-02-06 14:00:10 -0500

    added controller, connection.js, index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const recipesController = require('./controllers/recipes');
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'hbs');
+
+// app.use('/recipes', recipesController);
+
+app.listen(4000, () => {
+    console.log('we\'re listening on port 4000');
+});
